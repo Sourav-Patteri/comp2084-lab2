3221646fbeb6a254891db1527b2a5e13da509071
Sourav-Patteri <souravpatteri@gmail.com>
2020-01-16 20:00:45 -0500

    Add project files.

---
+++ Lab 2/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(string product)
+        {
+            ViewBag.Message = "You selected product:" + product;
+            return View();
+        }
+    }
+}
